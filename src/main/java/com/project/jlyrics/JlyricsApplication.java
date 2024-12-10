d5e787b1a2cbef72ea7c5692b502fb1150080d8e
Lily <helloimlily@gmail.com>
2024-12-10 15:18:36 +0900

    initial commit

---
+++ src/main/java/com/project/jlyrics/JlyricsApplication.java
@@ -0,0 +1,13 @@
+package com.project.jlyrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JlyricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JlyricsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/jlyrics/JlyricsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.jlyrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JlyricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
